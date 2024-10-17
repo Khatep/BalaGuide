0edd7954669c515ef56cb10195e9412fbe6e58c9
Khatep <aserty664qer@gmail.com>
2024-10-17 13:46:32 +0500

    Practice 1

---
+++ src/test/java/org/khatep/balaguide/BalaGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package org.khatep.balaguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalaGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
