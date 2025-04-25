3a1fae0cbb2fe8e1553c64614d417ed58e4744f9
nkhatep <nkhatep@silksoftgroup.com>
2025-04-25 16:05:04 +0500

    Replace temporary in-memory response metadata handling with database integration
    Switched from an in-memory `ResponseMessageRepositoryTemp` to `ResponseMessageRepository` backed by a database. Added SQL migration script to populate response metadata, and updated related services and exception handler to utilize the new implementation.

--- src/main/java/kz/balaguide/common_module/services/responsemetadata/ResponseMetadataServiceImpl.java
+++ src/main/java/kz/balaguide/common_module/services/responsemetadata/ResponseMetadataServiceImpl.java
@@ -2,17 +2,17 @@ package kz.balaguide.common_module.services.responsemetadata;
 
 import kz.balaguide.common_module.core.entities.ResponseMetadata;
 import kz.balaguide.common_module.core.enums.ResponseCode;
-import kz.balaguide.common_module.repositories.responsemessage.ResponseMessageRepositoryTemp;
+import kz.balaguide.common_module.repositories.responsemessage.ResponseMessageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ResponseMetadataServiceImpl implements ResponseMetadataService {
-    private final ResponseMessageRepositoryTemp responseMessageRepositoryTemp;
+    private final ResponseMessageRepository responseMessageRepository;
 
     @Override
     public ResponseMetadata findByCode(ResponseCode responseCode) {
-        return responseMessageRepositoryTemp.findByResponseCode(responseCode);
+        return responseMessageRepository.findByResponseCode(responseCode);
     }
 }
